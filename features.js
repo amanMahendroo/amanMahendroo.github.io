1262228d1489035336b01635c89a5a16c8b9558f
Aman Mahendroo <30946991+amanMahendroo@users.noreply.github.com>
2020-05-24 12:58:27 +0530

    Delete features.js

--- features.js
+++
@@ -1,8 +0,0 @@
-function feature(t, d) {
-	this.title = t || "Acoustic Guitar"
-	this.desc = d || "Get imported and branded guitars at affordable price."
-}
-
-for (var i = 0; i < 5; i++) {
-	features.push(new feature())
-}
